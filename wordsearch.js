06bc70cf3e6ae72337e6849f24eee8a4f619ab54
dorreb <ashley_ure@hotmail.com>
2023-01-18 17:43:41 -0500

    worked on with katie Liu

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,16 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (!letters.length) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let i = 0; i < letters[0].length; i++) {
+    let col = letters.map(row => row[i]).join("");
+    if (col.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
